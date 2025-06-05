5df3f223b9b8c47a11c55bb21c8d7f82013af0ab
Niloo Mont <niloo.mon@gmail.com>
2025-06-05 14:59:34 +0330

    fix: implement layout of course detail page

--- src/app/courses/components/course-detail/course-detail.component.ts
+++ src/app/courses/components/course-detail/course-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Messages } from '../../../texts/messages';
 
 @Component({
   selector: 'hw-course-detail',
@@ -7,8 +8,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './course-detail.component.css'
 })
 export class CourseDetailComponent {
+  Messages = Messages;
   courseId!: number;
   courseSlug!: string;
+  isVideoModalOpen = false;
+  videoUrl = 'videos/courses/1.mp4'; // your full video URL
+
+  @ViewChild('videoPlayer') videoPlayer?: ElementRef<HTMLVideoElement>;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -19,4 +25,16 @@ export class CourseDetailComponent {
     });
   }
 
+  openVideoModal() {
+    this.isVideoModalOpen = true;
+  }
+
+  closeVideoModal() {
+    this.isVideoModalOpen = false;
+    // pause video when modal closes
+    if (this.videoPlayer?.nativeElement) {
+      this.videoPlayer.nativeElement.pause();
+      this.videoPlayer.nativeElement.currentTime = 0;
+    }
+  }
 }
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CourseDetailComponent
+  ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
   ]
 })
 export class CoursesModule { }
