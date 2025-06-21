02d8d6fb9aa3f0e40c6b7342ec10c107c9a6be1f
Niloo Mont <niloo.mon@gmail.com>
2025-06-21 15:18:08 +0330

    fix: jumborton background image source

--- src/app/shared/components/jumbotron/jumbotron.component.ts
+++ src/app/shared/components/jumbotron/jumbotron.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Messages } from '../../../texts/messages';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'hw-jumbotron',
@@ -8,4 +9,5 @@ import { Messages } from '../../../texts/messages';
 })
 export class JumbotronComponent {
   Messages = Messages;
+  baseUrl = environment.apiBaseUrl;
 }
