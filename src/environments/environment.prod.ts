265b0619251b41b9c9444456caee00f62bfd9ed0
Niloo Mont <niloo.mon@gmail.com>
2025-08-01 15:36:56 +0330

    fix: change environment url to main website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://dev.omgpv.ir',
-    frontBaseUrl: 'https://dev.omgpv.ir'
+    apiBaseUrl: 'https://hardworker.ir',
+    frontBaseUrl: 'https://hardworker.ir'
 };
+
+// export const environment = {
+//     production: true,
+//     apiBaseUrl: 'https://dev.omgpv.ir',
+//     frontBaseUrl: 'https://dev.omgpv.ir'
+// };
