b1f01b7d63b665557c56322def58e0f013e2db71
Niloo Mont <niloo.mon@gmail.com>
2025-08-03 15:02:26 +0330

    fix: Card carousel not showing the last message fully bug

--- src/app/shared/components/card-carousel/card-carousel.component.ts
+++ src/app/shared/components/card-carousel/card-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { IUserCommentList } from '../../../models/IUserCommentList';
 
 @Component({
@@ -6,36 +6,36 @@ import { IUserCommentList } from '../../../models/IUserCommentList';
   templateUrl: './card-carousel.component.html',
   styleUrl: './card-carousel.component.css'
 })
-export class CardCarouselComponent {
+export class CardCarouselComponent implements AfterViewInit, OnDestroy {
   @Input() cards: IUserCommentList[] = [];
 
   @ViewChild('carousel') carousel!: ElementRef;
+  @ViewChildren('cardRef') cardElements!: QueryList<ElementRef>;
+
+  isReady: boolean = false;
+  translateX: string = 'translateX(0px)';
   index = 0;
   autoSlideInterval: any;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private cdr: ChangeDetectorRef) { }
 
   ngAfterViewInit() {
+    this.calculateTranslateX(0);
+    this.isReady = true;
+    this.cdr.detectChanges(); // Now safe
     this.startAutoSlide();
   }
 
   next() {
-    if (this.index < this.cards.length - 1) {
-      this.index++;
-    } else {
-      this.index = 0; // Loop back to first
-    }
-    this.updateCarousel();
+    this.index = (this.index + 1) % this.cards.length;
+    this.calculateTranslateX(this.index);
   }
 
   prev() {
-    if (this.index > 0) {
-      this.index--;
-    } else {
-      this.index = this.cards.length - 1; // Loop to last
-    }
-    this.updateCarousel();
+    this.index = (this.index - 1 + this.cards.length) % this.cards.length;
+    this.calculateTranslateX(this.index);
   }
+
 
   updateCarousel() {
     const translateValue = `translateX(${this.index * -65}%)`;
@@ -51,6 +51,35 @@ export class CardCarouselComponent {
   stopAutoSlide() {
     if (this.autoSlideInterval) {
       clearInterval(this.autoSlideInterval);
+    }
+  }
+
+  private calculateTranslateX(index: number): void {
+    if (!this.cardElements || !this.carousel) {
+      this.translateX = 'translateX(0px)';
+      return;
+    }
+
+    if (index == 0)
+      this.translateX = 'translateX(0px)';
+    else {
+      let totalOffset = 0;
+      for (let i = 0; i < index; i++) {
+        const el = this.cardElements.get(i)?.nativeElement;
+        if (el) {
+          totalOffset += el.getBoundingClientRect().width;
+        }
+      }
+
+      const containerWidth = this.carousel.nativeElement.offsetWidth;
+      let allCardsWidth = 0;
+      this.cardElements.forEach(elRef => {
+        allCardsWidth += elRef.nativeElement.getBoundingClientRect().width;
+      });
+
+      const maxTranslate = allCardsWidth - containerWidth;
+      const clamped = Math.min(totalOffset, maxTranslate);
+      this.translateX = `translateX(${clamped}px)`; // For RTL
     }
   }
 
