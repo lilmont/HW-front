e1f6f932cb821fd1c7f86f15335fa76f1fac0817
Niloo Mont <niloo.mon@gmail.com>
2025-08-03 20:35:37 +0330

    fix: show UserId i login codes list

--- src/app/admin/models/ILoginCodeListItem.ts
+++ src/app/admin/models/ILoginCodeListItem.ts
@@ -1,12 +1,15 @@
 export interface ILoginCodeListItem {
     code: string;
-    phoneNumber: string;
+    phoneNumber?: string;
+    userId?: string;
     dateCreated: string;
 }
 
 export class LoginCodeListItem implements ILoginCodeListItem {
     code: string = '';
-    phoneNumber: string = '';
+    phoneNumber?: string = '';
+    userId?: string;
+
     dateCreated: string = '';
     constructor(init?: Partial<LoginCodeListItem>) {
         Object.assign(this, init);
