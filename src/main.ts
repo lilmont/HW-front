4bff1dcc5412d6856948adca34945227381080bb
Niloo Mont <niloo.mon@gmail.com>
2025-08-29 15:49:01 +0330

    fix: register chartjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { CategoryScale, Chart, Filler, Legend, LinearScale, LineController, LineElement, PointElement, Title, Tooltip } from 'chart.js';
+import { provideCharts } from 'ng2-charts';
+
+Chart.register(LineController, LinearScale, CategoryScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+  ngZoneEventCoalescing: true,
+  providers: [
+    provideCharts() // Add ng2-charts provider
+  ]
+}).catch(err => console.error(err));
