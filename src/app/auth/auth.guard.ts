011587119f34da767aa577568f3f6e0b0a355511
Niloo Mont <niloo.mon@gmail.com>
2025-08-30 14:29:49 +0330

    fix: return url for authguard in dashboard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { JwtHelperService } from "../core/services/jwt.helper.service";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(private jwtHelper: JwtHelperService, private router: Router) { }
 
-    canActivate(): boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         if (this.jwtHelper.isLoggedIn()) return true;
 
-        this.router.navigate(['/users/signup']);
+        this.router.navigate(['/users/signup'], { queryParams: { returnUrl: state.url } });
         return false;
     }
 }
